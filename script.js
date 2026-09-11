c91af7587689be67656574957c018f37ffd4e8d8
boris283 <b75193996@gmail.com>
2026-09-11 10:11:02 +0100

    Ajout du menu hamburger mobile

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ const observateur = new IntersectionObserver((entrees) => {
 sections.forEach((section) => {
     observateur.observe(section);
 });
+
+const boutonHamburger = document.getElementById('btn-hamburger');
+const listeLiens = document.getElementById('nav-links');
+
+boutonHamburger.addEventListener('click', () => {
+    listeLiens.classList.toggle('ouvert');
+});
+
+const liens = document.querySelectorAll('.nav-links a');
+
+liens.forEach((lien) => {
+    lien.addEventListener('click', () => {
+        listeLiens.classList.remove('ouvert');
+    });
+});
